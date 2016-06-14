e70fd4ca1d4b4e81dd9374fde41731182957c1f7
taomingwang <taojike@126.com>
2016-06-14 21:58:52 +0800

    学习vue

---
+++ js/waterfall.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    var example1 = new Vue({
+        el: '#example-1',
+        data: {
+            items: [
+                { message: 'Foo' },
+                { message: 'Bar' }
+            ]
+        }
+    })
+}
